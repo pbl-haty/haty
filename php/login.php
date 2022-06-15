27bd7c28cb5535ee70adfdafbed341c2c0deb522
Mitsui8810 <showdown3218810@gmail.com>
2022-06-15 10:48:49 +0900

    login.php indent

--- php/login.php
+++ php/login.php
@@ -8,12 +8,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
           integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
           crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Haty</title>
+    <title>login</title>
 </head>
 <body>
     <input type="text" class="id" placeholder="メールアドレス">&ensp;&ensp;&ensp;<br>
     <input type="password" class="pass" placeholder="パスワード">
     <i id="eye" class="fa-solid fa-eye"></i>
+
+    <!-- パスワード確認動作 -->
     <script>
         let eye = document.getElementById("eye");
         eye.addEventListener('click', function () {
@@ -27,25 +29,27 @@
                   this.classList.toggle('fa-eye-slash');
              }
         })
-   </script>
+    </script>
+
     <div>
         <input type="checkbox" id="login" class="login">
         <label for="login">次回から自動ログイン</label>
     </div>
+
     <button class="btn-login" id="check">login</button><br>
+    
     <script>
         $(document).ready(function() {
-        $('#check').click(function() {
-        var val1st = $('#id').val();
-        if (!val1st) {
-            alert("テキストが入力されていません。")
-            return false;
-        }
-    });
-});
-
+            $('#check').click(function() {
+                var val1st = $('#id').val();
+                if (!val1st) {
+                    alert("テキストが入力されていません。")
+                return false;
+                }
+            });
+        });
     </script>
-    <a href="#" class="lnk-sakusei">アカウントを作成</a>
-    
+
+    <a href="#" class="lnk-sakusei">アカウントを作成</a>  
 </body>
 </html>
