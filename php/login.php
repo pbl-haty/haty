81142c1a956dbbd38b7a3720f438d42a1b7d4853
Mitsui8810 <showdown3218810@gmail.com>
2022-06-15 13:20:34 +0900

    dbdata.php created

--- php/login.php
+++ php/login.php
@@ -1,3 +1,49 @@
+<?php
+    // セッションの開始
+    session_start();
+
+    $db['host'] = "localhost";  // DBサーバのURL
+    $db['user'] = "haty";       // ユーザー名
+    $db['pass'] = "haty";       // ユーザー名のパスワード
+    $db['dbname'] = "haty";     // データベース名
+
+    // エラーメッセージの初期化
+    $php_errormsg = '';
+
+    // ログインボタンが押されたとき
+    if(isset($_POST['login'])){
+        // 入力されたメールアドレスとパスワードのチェック
+        if(empty($_POST['login_email'])){
+            $php_errormsg = "メールアドレスが入力されていません";
+        }else if(empty($_POST['login_pass'])){
+            $php_errormsg = "パスワードが入力されていません";
+        }
+
+        // メールアドレスとパスワードが入力されている場合
+        if(!empty($_POST['login_email']) && !empty($_POST['login_pass'])){
+            // 入力されたメールアドレスを変数に格納
+            $login_email = $_POST['login_email'];
+
+            // PDOオブジェクトの生成
+            $dsn = 'mysql:host=localhost;dbname=login;charset=utf8';
+            $pdo = new PDO($dsn, $db['pass'], $db['dbname'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
+            // エラー処理
+            try{
+
+            }catch(PDOException $e){
+                $php_errormsg = "データベースのエラーで発生しました";
+            }
+        }
+    }
+?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="jp">
 <head>
@@ -11,33 +57,36 @@
     <title>login</title>
 </head>
 <body>
-    <input type="text" class="id" placeholder="メールアドレス">&ensp;&ensp;&ensp;<br>
-    <input type="password" class="pass" placeholder="パスワード">
-    <i id="eye" class="fa-solid fa-eye"></i>
+    <form action="">
+        <input type="text" class="id" name="login_email" placeholder="メールアドレス">&ensp;&ensp;&ensp;<br>
+        <input type="password" class="pass" name="login_pass" placeholder="パスワード">
+        <i id="eye" class="fa-solid fa-eye"></i>
 
-    <!-- パスワード確認動作 -->
-    <script>
-        let eye = document.getElementById("eye");
-        eye.addEventListener('click', function () {
-             if (this.previousElementSibling.getAttribute('type') == 'password') {
-                  this.previousElementSibling.setAttribute('type', 'text');
-                  this.classList.toggle('fa-eye');
-                  this.classList.toggle('fa-eye-slash');
-             } else {
-                  this.previousElementSibling.setAttribute('type', 'password');
-                  this.classList.toggle('fa-eye');
-                  this.classList.toggle('fa-eye-slash');
-             }
-        })
-    </script>
+        <!-- パスワード確認動作 -->
+        <script>
+            let eye = document.getElementById("eye");
+            eye.addEventListener('click', function () {
+                if (this.previousElementSibling.getAttribute('type') == 'password') {
+                    this.previousElementSibling.setAttribute('type', 'text');
+                    this.classList.toggle('fa-eye');
+                    this.classList.toggle('fa-eye-slash');
+                } else {
+                    this.previousElementSibling.setAttribute('type', 'password');
+                    this.classList.toggle('fa-eye');
+                    this.classList.toggle('fa-eye-slash');
+                }
+            })
+        </script>
 
-    <div>
-        <input type="checkbox" id="login" class="login">
-        <label for="login">次回から自動ログイン</label>
-    </div>
+        <div>
+            <input type="checkbox" id="login" class="login">
+            <label for="login">次回から自動ログイン</label>
+        </div>
 
-    <button class="btn-login" id="check">login</button><br>
-    
+        <input type="button" class="btn-login" id="check" name="login" value="login">
+        <br>
+    </form>
+
     <script>
         $(document).ready(function() {
             $('#check').click(function() {
