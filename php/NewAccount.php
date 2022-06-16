806f7830bf576177acdd3caf435b212c50e0ba3d
Mitsui8810 <showdown3218810@gmail.com>
2022-06-16 11:20:35 +0900

    revise html

--- php/NewAccount.php
+++ php/NewAccount.php
@@ -5,15 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/NewAccount.css">
-    <title>Haty</title>
+    <title>新規アカウント登録</title>
 </head>
 <body>
+    <h1>新規会員登録</h1>
     <form action="" method="post">
-    <div><input type="text" class="name" placeholder="ユーザーネーム"></div>
-    <div><input type="text" class="id" placeholder="メールアドレス"></div>
-    <div><input type="password" class="pass" placeholder="パスワード"></div>
-    <div><input type="password" class="check" placeholder="パスワード"></div>
+        <div>
+            <input type="text" class="name" name="signup_name" placeholder="ユーザーネーム">
+        </div>
+        <div>
+            <input type="email" class="id" name="signup_email" placeholder="メールアドレス">
+        </div>
+        <div>
+            <input type="password" class="pass" placeholder="パスワード">
+        </div>
+        <div>
+            <input type="password" class="check" placeholder="パスワード再入力">
+        </div>
+        <input type="submit" class="btn-create" value="アカウント登録">
     </form>
-    <div><button class="btn-create">作成</button></div>
 </body>
 </html>
