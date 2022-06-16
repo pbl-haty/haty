dd30bb5425dec4890b3e9650fe20b7bb12640c87
Mitsui8810 <showdown3218810@gmail.com>
2022-06-16 11:58:23 +0900

    added signUp method

--- php/classes/user.php
+++ php/classes/user.php
@@ -9,5 +9,31 @@
             $stmt = $this->query($sql, [$mailaddress, $password]);
             return $stmt->fetch();
         }
+
+
+        // ユーザー登録処理
+        public function signUp($name, $password, $mailaddress){
+            // 既にメールアドレスが登録されているかの確認
+            $sql = "select * from user where mailaddress = ?";
+            $stmt = $this->query($sql, [$mailaddress]);
+            $result = $stmt->fetch();
+
+            // アカウント登録するメールアドレスのアカウントが既に登録されているとき
+            if($result){
+                return 'この' . $mailaddress . 'は既に登録されています。';
+            }
+
+            // 入力された情報をもとに、データベースにアカウント、ユーザーの追加
+            $sql = 'insert into user(name, password, mailaddress) values(?, ?, ?)';
+            $result= $this->exec($sql, [$name, $password, $mailaddress]);
+
+            if($result){
+                // 登録に成功した場合
+                return '';
+            }else{
+                // 何かしらの原因で失敗した場合
+                return '新規アカウント登録できませんでした。管理者にお問い合わせください。';
+            }
+        }
     }
 ?>
