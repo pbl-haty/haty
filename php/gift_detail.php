744528d7614ff48319f9bca66d565e02d5524207
takuma <kd1206659@st.kobedenshi.ac.jp>
2022-06-22 09:48:49 +0900

    詳細画面修正

--- php/gift_detail.php
+++ php/gift_detail.php
@@ -18,168 +18,74 @@ $userId = $_SESSION['uid'];
 
 
 <body>
-    <h2 class="syosai">詳細画面</h2>
+    <h2 class="syosai">ギフト詳細画面</h2>
 
     <ul class="slider">
-        <!-- ページ始まり -->
-        <div>
-            <li><img class="gift_display_detail" src="スイッチのジョイコン.jpg" alt=""></li>
-            <div class="gift_sentence">
-                <p class="gift_name">ギフト名：</p>
-                <p class="gift_name">スイッチのジョイコン</p>
-            </div>
-            <div class="gift_sentence">
-                <p class="gift_name">投稿者名：</p>
-                <p class="gift_name">神戸太郎</p>
-            </div>
-            <hr>
-            <p class="explain_sentence">説明</p>
-            <p class="once_sentence">スイッチのジョイコンです。～のボタンが効かなくなってしまっているため投稿しました。だれでもいいので貰ってください。</p>
-            <hr>
-            <div class="good-request_count">
-                <p class="good-request_sentence">いいね👍</p>
-                <p class="good-request_number">1</p>
-            </div>
-            <div class="good-request_count">
-                <p class="good-request_sentence">申請者📃</p>
-                <p class="good-request_number">2</p>
-            </div>
-            <hr>
-            <p class="comment_sentence">コメント</p>
-
-        </div>
-        <!-- ページ終わり -->
-
-        <!-- ページ始まり -->
-        <div>
-            <li><img class="gift_display_detail" src="カボスのぬいぐるみ.jpg" alt=""></li>
-            <div class="gift_sentence">
-                <p class="gift_name">ギフト名：</p>
-                <p class="gift_name">カボスのぬいぐるみ</p>
-            </div>
-            <div class="gift_sentence">
-                <p class="gift_name">投稿者名：</p>
-                <p class="gift_name">神戸太郎</p>
-            </div>
-            <hr>
-            <p class="explain_sentence">説明</p>
-            <p class="once_sentence">スイッチのジョイコンです。～のボタンが効かなくなってしまっているため投稿しました。だれでもいいので貰ってください。</p>
-            <hr>
-            <div class="good-request_count">
-                <p class="good-request_sentence">いいね👍</p>
-                <p class="good-request_number">1</p>
-            </div>
-            <div class="good-request_count">
-                <p class="good-request_sentence">申請者📃</p>
-                <p class="good-request_number">2</p>
-            </div>
-            <hr>
-            <p class="comment_sentence">コメント</p>
-
-        </div>
-        <!-- ページ終わり -->
-
-        <!-- ページ始まり -->
-        <div>
-            <li><img class="gift_display_detail" src="USBケーブル.jpg" alt=""></li>
-            <div class="gift_sentence">
-                <p class="gift_name">ギフト名：</p>
-                <p class="gift_name">USBケーブル</p>
-            </div>
-            <div class="gift_sentence">
-                <p class="gift_name">投稿者名：</p>
-                <p class="gift_name">神戸太郎</p>
-            </div>
-            <hr>
-            <p class="explain_sentence">説明</p>
-            <p class="once_sentence">スイッチのジョイコンです。～のボタンが効かなくなってしまっているため投稿しました。だれでもいいので貰ってください。</p>
-            <hr>
-            <div class="good-request_count">
-                <p class="good-request_sentence">いいね👍</p>
-                <p class="good-request_number">1</p>
-            </div>
-            <div class="good-request_count">
-                <p class="good-request_sentence">申請者📃</p>
-                <p class="good-request_number">2</p>
-            </div>
-            <hr>
-            <p class="comment_sentence">コメント</p>
-
-        </div>
-        <!-- ページ終わり -->
-
-        <!-- ページ始まり -->
-        <div>
-            <li><img class="gift_display_detail" src="カプセル薬.jpg" alt=""></li>
-            <div class="gift_sentence">
-                <p class="gift_name">ギフト名：</p>
-                <p class="gift_name">カプセル薬</p>
-            </div>
-            <div class="gift_sentence">
-                <p class="gift_name">投稿者名：</p>
-                <p class="gift_name">神戸太郎</p>
-            </div>
-            <hr>
-            <p class="explain_sentence">説明</p>
-            <p class="once_sentence">スイッチのジョイコンです。～のボタンが効かなくなってしまっているため投稿しました。だれでもいいので貰ってください。</p>
-            <hr>
-            <div class="good-request_count">
-                <p class="good-request_sentence">いいね👍</p>
-                <p class="good-request_number">1</p>
-            </div>
-            <div class="good-request_count">
-                <p class="good-request_sentence">申請者📃</p>
-                <p class="good-request_number">2</p>
-            </div>
-            <hr>
-            <p class="comment_sentence">コメント</p>
-
-        </div>
-        <!-- ページ終わり -->
-
-        <!-- ページ始まり -->
-        <div>
-            <li><img class="gift_display_detail" src="AirPad.jpg" alt=""></li>
-            <div class="gift_sentence">
-                <p class="gift_name">ギフト名：</p>
-                <p class="gift_name">AirPad</p>
-            </div>
-            <div class="gift_sentence">
-                <p class="gift_name">投稿者名：</p>
-                <p class="gift_name">神戸太郎</p>
-            </div>
-            <hr>
-            <p class="explain_sentence">説明</p>
-            <p class="once_sentence">スイッチのジョイコンです。～のボタンが効かなくなってしまっているため投稿しました。だれでもいいので貰ってください。</p>
-            <hr>
-            <div class="good-request_count">
-                <p class="good-request_sentence">いいね👍</p>
-                <p class="good-request_number">1</p>
-            </div>
-            <div class="good-request_count">
-                <p class="good-request_sentence">申請者📃</p>
-                <p class="good-request_number">2</p>
-            </div>
-            <hr>
-            <!--吹き出しはじまり-->
-            <p class="comment_sentence">コメント</p>
-            <div class="balloon5">
-                <div class="faceicon">
-                    <img>
-                </div>
-                <div class="chatting">
-                    <div class="says">
-                        <p>コメントコメントコメント</p>
-                    </div>
-                </div>
-            </div>
-            <!--吹き出し終わり-->
-
-        </div>
-        <!-- ページ終わり -->
-
-        <!--/slider-->
+        <li><img class="gift_display_detail" src="" alt=""></li>
+        <li><img class="gift_display_detail" src="" alt=""></li>
+        <li><img class="gift_display_detail" src="" alt=""></li>
     </ul>
+    <p class="gift_name">AirPad</p>
+    <br>
+    <p class="gift-t_name">神戸太郎</p>
+    <p class="gift_connect">が投稿</p>
+    <br>
+    <div class="good_count">
+        <div class="good_sentence">
+            <input type="checkbox" id="2" name="good_sentence"><label for="2">💗いいね</label>
+        </div>
+        <div class="good_number">
+            <p>いいね数</p>
+            <p>1</p>
+        </div>
+    </div>
+    <hr>
+    <p class="explain_sentence">説明</p>
+    <p class="once_sentence">スイッチのジョイコンです。～のボタンが効かなくなってしまっているため投稿しました。だれでもいいので貰ってください。</p>
+    <div class="gift_sentence">
+        <p class="gift_category">手渡し・郵送</p>
+        <p class="gift_category">電子機器</p>
+    </div>
+    <p class="gift_date">2022年6月25日</p>
+    <hr>
+    <div class="gift_sentence">
+        <p class="request_possible">申請可</p><input type="button" class="request_sentence" value="ギフト申請へ">
+    </div>
+
+    <hr>
+
+    <!--吹き出しはじまり-->
+    <p class="comment_sentence">コメント</p>
+    <div class="chatting_place">
+        <div class="faceicon">
+            <!-- アイコンor名前？ -->
+        </div>
+        <div class="chatting">
+            <div class="says">
+                <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
+            </div>
+            <input type="button" class="comment_reply" value="返信">
+            <div class="says">
+                <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
+            </div>
+            <input type="button" class="comment_reply" value="返信">
+            <div class="says">
+                <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
+            </div>
+            <input type="button" class="comment_reply" value="返信">
+        </div>
+    </div>
+    <br>
+    <!--吹き出し終わり-->
+
+    <!-- コメント入力 -->
+    <div>
+        <p>コメントを入力</p>
+        <textarea class="comment_box" placeholder="コメントを入力してください"></textarea>
+    </div>
+    <br>
+    <input type="submit" class="comment-send_btn" value="送信">
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
