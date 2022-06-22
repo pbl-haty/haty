ed94042da23b59fae402984f0bf6b77444e60718
takuma <kd1206659@st.kobedenshi.ac.jp>
2022-06-22 10:07:26 +0900

    パディング修正

--- php/gift_detail.php
+++ php/gift_detail.php
@@ -18,73 +18,75 @@ $userId = $_SESSION['uid'];
 
 
 <body>
-    <h2 class="syosai">ギフト詳細画面</h2>
+    <div class="gift_padding">
+        <h2 class="syosai">ギフト詳細画面</h2>
 
-    <ul class="slider">
-        <li><img class="gift_display_detail" src="" alt=""></li>
-        <li><img class="gift_display_detail" src="" alt=""></li>
-        <li><img class="gift_display_detail" src="" alt=""></li>
-    </ul>
-    <p class="gift_name">AirPad</p>
-    <br>
-    <p class="gift-t_name">神戸太郎</p>
-    <p class="gift_connect">が投稿</p>
-    <br>
-    <div class="good_count">
-        <div class="good_sentence">
-            <input type="checkbox" id="2" name="good_sentence"><label for="2">💗いいね</label>
-        </div>
-        <div class="good_number">
-            <p>いいね数</p>
-            <p>1</p>
-        </div>
-    </div>
-    <hr>
-    <p class="explain_sentence">説明</p>
-    <p class="once_sentence">スイッチのジョイコンです。～のボタンが効かなくなってしまっているため投稿しました。だれでもいいので貰ってください。</p>
-    <div class="gift_sentence">
-        <p class="gift_category">手渡し・郵送</p>
-        <p class="gift_category">電子機器</p>
-    </div>
-    <p class="gift_date">2022年6月25日</p>
-    <hr>
-    <div class="gift_sentence">
-        <p class="request_possible">申請可</p><input type="button" class="request_sentence" value="ギフト申請へ">
-    </div>
-
-    <hr>
-
-    <!--吹き出しはじまり-->
-    <p class="comment_sentence">コメント</p>
-    <div class="chatting_place">
-        <div class="faceicon">
-            <!-- アイコンor名前？ -->
-        </div>
-        <div class="chatting">
-            <div class="says">
-                <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
+        <ul class="slider">
+            <li><img class="gift_display_detail" src="" alt=""></li>
+            <li><img class="gift_display_detail" src="" alt=""></li>
+            <li><img class="gift_display_detail" src="" alt=""></li>
+        </ul>
+        <p class="gift_name">AirPad</p>
+        <br>
+        <p class="gift-t_name">神戸太郎</p>
+        <p class="gift_connect">が投稿</p>
+        <br>
+        <div class="good_count">
+            <div class="good_sentence">
+                <input type="checkbox" id="2" name="good_sentence"><label for="2">💗いいね</label>
             </div>
-            <input type="button" class="comment_reply" value="返信">
-            <div class="says">
-                <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
+            <div class="good_number">
+                <p>いいね数</p>
+                <p>1</p>
             </div>
-            <input type="button" class="comment_reply" value="返信">
-            <div class="says">
-                <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
-            </div>
-            <input type="button" class="comment_reply" value="返信">
         </div>
-    </div>
-    <br>
-    <!--吹き出し終わり-->
+        <hr>
+        <p class="explain_sentence">説明</p>
+        <p class="once_sentence">スイッチのジョイコンです。～のボタンが効かなくなってしまっているため投稿しました。だれでもいいので貰ってください。</p>
+        <div class="gift_sentence">
+            <p class="gift_category">手渡し・郵送</p>
+            <p class="gift_category">電子機器</p>
+        </div>
+        <p class="gift_date">2022年6月25日</p>
+        <hr>
+        <div class="gift_sentence">
+            <p class="request_possible">申請可</p><input type="button" class="request_sentence" value="ギフト申請へ">
+        </div>
 
-    <!-- コメント入力 -->
-    <div>
-        <p>コメントを入力</p>
-        <textarea class="comment_box" placeholder="コメントを入力してください"></textarea>
+        <hr>
+
+        <!--吹き出しはじまり-->
+        <p class="comment_sentence">コメント</p>
+        <div class="chatting_place">
+            <div class="faceicon">
+                <!-- アイコンor名前？ -->
+            </div>
+            <div class="chatting">
+                <div class="says">
+                    <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
+                </div>
+                <input type="button" class="comment_reply" value="返信">
+                <div class="says">
+                    <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
+                </div>
+                <input type="button" class="comment_reply" value="返信">
+                <div class="says">
+                    <p>コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</p>
+                </div>
+                <input type="button" class="comment_reply" value="返信">
+            </div>
+        </div>
+        <br>
+        <!--吹き出し終わり-->
+
+        <!-- コメント入力 -->
+        <div>
+            <p>コメントを入力</p>
+            <textarea class="comment_box" placeholder="コメントを入力してください"></textarea>
+        </div>
+        <br>
+        <input type="submit" class="comment-send_btn" value="送信">
     </div>
-    <br>
-    <input type="submit" class="comment-send_btn" value="送信">
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
