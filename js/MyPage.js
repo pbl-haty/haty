c87e346b4419b426f51ab9eacd2b9a5ab334917f
Yokota Atsuya <kd1248793@st.kobedenshi.ac.jp>
2022-06-28 11:14:57 +0900

    マイページ一覧機能完成

--- js/MyPage.js
+++ js/MyPage.js
@@ -1,12 +1,54 @@
 p0.style.display ="flex";
 p1.style.display ="none";
+p2.style.display ="none";
+p3.style.display ="none";
+p4.style.display ="none";
+p5.style.display ="none";
 
 function click_list_event(i) {
     if(i == 0) {
         p0.style.display ="flex";
         p1.style.display ="none";
+        p2.style.display ="none";
+        p3.style.display ="none";
+        p4.style.display ="none";
+        p5.style.display ="none";
     } else if(i == 1) {
         p0.style.display ="none";
         p1.style.display ="flex";
+        p2.style.display ="none";
+        p3.style.display ="none";
+        p4.style.display ="none";
+        p5.style.display ="none";
+    } else if(i == 2) {
+        p0.style.display ="none";
+        p1.style.display ="none";
+        p2.style.display ="flex";
+        p3.style.display ="none";
+        p4.style.display ="none";
+        p5.style.display ="none";
+    } else if(i == 3) {
+        p0.style.display ="none";
+        p1.style.display ="none";
+        p2.style.display ="none";
+        p3.style.display ="flex";
+        p4.style.display ="none";
+        p5.style.display ="none";
+    }
+    else if(i == 4) {
+        p0.style.display ="none";
+        p1.style.display ="none";
+        p2.style.display ="none";
+        p3.style.display ="none";
+        p4.style.display ="flex";
+        p5.style.display ="none";
+    }
+    else if(i == 5) {
+        p0.style.display ="none";
+        p1.style.display ="none";
+        p2.style.display ="none";
+        p3.style.display ="none";
+        p4.style.display ="none";
+        p5.style.display ="flex";
     }
 }
--- php/classes/getdata.php
+++ php/classes/getdata.php
@@ -5,17 +5,52 @@
     class GetData extends DbData {
     // 商品をカートに入れる ・・ テーブルcartに登録する
 
-        public function goodlist($userId){
+        // 自分が「いいね」している商品を取得(未取引の物のみ表示)
+        public function mygoodlist($userId){
             $sql = "select gift.id, gift.gift_name, gift.image, gift.post
                     from good join gift on good.gift_id = gift.id
-                    where good.user_id = ?
+                    where good.user_id = ? and gift.judge is null
+                    order by gift.post desc, gift.id desc";
+            $stmt = $this->query($sql, [$userId]);
+            $items = $stmt->fetchAll();
+            return $items;
+        }
+            
+        // 自分が申請して成立した商品の履歴
+        public function myjudgelist($userId){
+            $sql = "select gift.id, gift.gift_name, gift.image, gift.post
+                    from gift
+                    where gift.applicant = ? and gift.judge is not null
+                    order by gift.post desc, gift.id desc";
+            $stmt = $this->query($sql, [$userId]);
+            $items = $stmt->fetchAll();
+            return $items;
+        }
+            
+        // 自分が相手に対して申請中の商品
+        public function myapplicationlist($userId){
+            $sql = "select gift.id, gift.gift_name, gift.image, gift.post
+                    from gift
+                    where gift.judge is null and gift.applicant = ?
                     order by gift.post desc, gift.id desc";
             $stmt = $this->query($sql, [$userId]);
             $items = $stmt->fetchAll();
             return $items;
         }
 
-        public function judgelist($userId){
+        // 相手が「いいね」している商品を取得(未取引の物のみ表示)
+        public function yourgoodlist($userId){
+            $sql = "select gift.id, gift.gift_name, gift.image, gift.post
+                    from good join gift on good.gift_id = gift.id
+                    where gift.user_id = ? and gift.judge is null
+                    order by gift.post desc, gift.id desc";
+            $stmt = $this->query($sql, [$userId]);
+            $items = $stmt->fetchAll();
+            return $items;
+        }
+
+        // 相手が申請して成立した商品の履歴
+        public function yourjudgelist($userId){
             $sql = "select gift.id, gift.gift_name, gift.image, gift.post
                     from gift
                     where gift.user_id = ? and gift.judge is not null
@@ -25,4 +60,14 @@
             return $items;
         }
 
+        // 相手が自分に対して申請中の商品
+        public function yourapplicationlist($userId){
+            $sql = "select gift.id, gift.gift_name, gift.image, gift.post
+                    from gift
+                    where gift.user_id = ? and gift.judge is null and gift.applicant is not null
+                    order by gift.post desc, gift.id desc";
+            $stmt = $this->query($sql, [$userId]);
+            $items = $stmt->fetchAll();
+            return $items;
+        }
     }
