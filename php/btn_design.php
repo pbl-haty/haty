9f7d1e02fa73e0e02aadf79de5b5cbd50fa61647
takuma <kd1206659@st.kobedenshi.ac.jp>
2022-06-29 14:49:41 +0900

    ボタンデザイン

---
+++ php/btn_design.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<link rel="stylesheet" href="../css/btn_design.css">
+
+<body>
+    <p>既存のボタン</p>
+    <input type="submit" class="btn-login" id="check" name="login" value="ログイン"><br><br>
+    <button type="submit" class="comment-send_btn" name="send_comment">送信</button><br><br>
+    <input type="submit" class="leave-btn" value="脱退"></p><br><br>
+    <input type="submit" class="edit_btn" name="edit_btn" value="編集"></p><br><br>
+    <button class="btn-create btn-style" style="margin-top: 100px;">作成</button><br><br>
+    <!-- 以下追加デザイン -->
+    <a href="#" class="sample01">送信</a><br><br>
+    <div class="sample02">
+        <a href="#" class="sample02">送信</a>
+    </div><br>
+    <div class="sample03">
+        <a href="#" class="sample03">削除</a>
+    </div>
+</body>
+
+</html>
+
+
+<!-- 参考サイト:
+https://www.yuu-diaryblog.com/2017/04/22/css-button-2/
+https://125naroom.com/web/2846 
+https://dubdesign.net/download/html-css/button-design/#-->
