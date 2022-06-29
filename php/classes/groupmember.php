b42fbf5cec0723fc06d10173b0079992fc78d81b
YokotaAtsuyakd <kd1248793@st.kobedenshi.ac.jp>
2022-06-29 15:11:48 +0900

    6/29

---
+++ php/classes/groupmember.php
@@ -0,0 +1,26 @@
+<?php
+
+    require_once __DIR__ . '/dbdata.php';
+
+    class GroupMember extends DbData {
+
+        // グループ情報取得
+        public function groupconf($groupId){
+            $sql = "select * from groupdb where id = ?";
+            $stmt = $this->query($sql, [$groupId]);
+            $item = $stmt->fetch();
+            return $item;
+        }
+
+        // グループメンバー表示
+        public function member($groupId){
+            $sql = "select user.uid, user.name
+                    from groupjoin join user on groupjoin.user_id = user.uid
+                    where groupjoin.group_id = ?
+                    order by user.uid desc";
+            $stmt = $this->query($sql, [$groupId]);
+            $items = $stmt->fetchAll();
+            return $items;
+        }
+
+    }
