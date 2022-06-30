e9827c905eea56eb882fd5a8270e13c1aaa9b007
YAMATO Matsuoka <kd1283950@st.kobedenshi.ac.jp>
2022-06-30 10:25:35 +0900

    画面追加

---
+++ php/GroupJoinsub.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="../css/GroupJoin.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="join-title">右端のやつら</h1>
+    <form action="">
+    <img src="../static/user_icon.png" class="img-join" alt="">
+    <div><input type="text" class="input-style" placeholder="グループコード"></div>
+    <div><input type="password" class="input-style" placeholder="パスワード"></div>
+    <button class="btn-join btn-style">参加</button>
+    </form>
+</body>
+</html>
