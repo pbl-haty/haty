70556ff90c3c5cb09ec078536a27bf9e8887c237
YAMATO Matsuoka <kd1283950@st.kobedenshi.ac.jp>
2022-07-08 14:42:54 +0900

    途中経過

--- php/GiftPost.php
+++ php/GiftPost.php
@@ -72,28 +72,32 @@
         <div class ="prompt_2">
                 <h4><?= $completionmsg ?></h4>
         </div>
+        <button type="button" class="next" style="width: 100px;height: 100px;">次</button>
+        <div class="box1" id="box1">
+            <h1 class="content-margin">商品画像</h1>
 
-        <h1 class="content-margin">商品画像</h1>
+            <div id="sample-img" class="sample-img"></div>
+                <label class="upload-label">
+                    画像を選択
+                    <input type="file" id="input-img" onchange="loadImage(this);" name="image[]" accept="image/*" multiple required>
+                </label>
 
-        <div id="sample-img" class="sample-img"></div>
-            <label class="upload-label">
-                画像を選択
-                <input type="file" id="input-img" onchange="loadImage(this);" name="image[]" accept="image/*" multiple required>
-            </label>
+            <h1 class="content-margin">商品名</h1>
 
-        <h1 class="content-margin">商品名</h1>
-
-        <div>
-            <input type="text" class="gift-title" maxlength="30" name="gift_name" required>
+            <div>
+                <input type="text" class="gift-title" maxlength="30" name="gift_name" required>
+            </div>
         </div>
 
-        <h1 class="content-margin">公開範囲</h1>
+        <div class="box2" id="box2">
+            <h1 class="content-margin">グループを選択</h1>
 
-        <div class ="prompt_2">
-            <h4><?= $errormsg1 ?></h4>
+            <div class ="prompt_2">
+                <h4><?= $errormsg1 ?></h4>
+            </div>
+
+            <div class="content-check">
         </div>
-
-        <div class="content-check">
 
 <?php 
     $group = new Group();
@@ -117,14 +121,15 @@
  ?>
 
         </div>
+        <div class="box3" id="box3">
+            <h1 class="content-margin">カテゴリ</h1>
 
-        <h1 class="content-margin">カテゴリ</h1>
-
-        <div class ="prompt_2">
-            <h4><?= $errormsg3 ?></h4>
+            <div class ="prompt_2">
+                <h4><?= $errormsg3 ?></h4>
+            </div>
         </div>
-
-        <div class="content-check">
+            <div class="content-check">
+        
 
 <?php 
     $gift_category = $post->giftcategory();
@@ -142,25 +147,28 @@
     }
  ?>
 
-        <h1 class="content-margin">取引条件</h1>
-        <div class ="prompt_2">
-            <h4><?= $errormsg2 ?></h4>
-        </div>
+            </div>
+        <div class="box4" id="box4">
+            <h1 class="content-margin">取引条件</h1>
+            <div class ="prompt_2">
+                <h4><?= $errormsg2 ?></h4>
+            </div>
 
-        <div class="content-check">
-            <div class="trade-box">
-                <input type="checkbox" id="trade1-conditions" name="conditions[]" value="1">
-                <label for="trade1-conditions">手渡し</label>
+            <div class="content-check">
+                <div class="trade-box">
+                    <input type="checkbox" id="trade1-conditions" name="conditions[]" value="1">
+                    <label for="trade1-conditions">手渡し</label>
+                </div>
+                <div class="trade-box">
+                    <input type="checkbox" id="trade2-conditions" name="conditions[]" value="2">
+                    <label for="trade2-conditions">配送</label>
+                </div>
             </div>
-            <div class="trade-box">
-                <input type="checkbox" id="trade2-conditions" name="conditions[]" value="2">
-                <label for="trade2-conditions">配送</label>
-            </div>
+            
+            <h1 class="content-margin">詳細情報（任意）</h1>
+            
+            <textarea class="Detailed-information" rows="5" name="gift_comment" value=""></textarea>
         </div>
-        
-        <h1 class="content-margin">詳細情報（任意）</h1>
-        
-        <textarea class="Detailed-information" rows="5" name="gift_comment" value=""></textarea>
         <br>
         <input type="submit" name="giftpost" class="form-btn" value="公開">
     </form>
@@ -170,5 +178,55 @@
 
     <script type="text/javascript" src="../js/giftpost.js"></script>
 
+    <script>
+/*         const btn = document.querySelector('.next');
+        const $box1 = document.querySelector(".box1");
+        const $box11 = document.querySelector('#box1');
+        const $left0 = parseInt($box1.style.left); */
+        
+        var $box1 = document.getElementById('box1');
+        var $box2 = document.getElementById('box2');
+        var $box3 = document.getElementById('box3');
+        var $box4 = document.getElementById('box4');
+        const btn = document.querySelector('.next');
+        $box1.style.left = "100%";
+        $box2.style.left = "200%";
+        $box3.style.left = "300%";
+        $box4.style.left = "400%";
+
+        const $left0 = parseInt($box1.style.left);
+        console.log($box1.style.left);
+        console.log($box1.style.left);
+        console.log($left0 - 100);
+        
+        btn.addEventListener('click', () => {
+        /* box.classList.toggle('next') */
+        $box1.style.left= (parseInt($box1.style.left) - 100) + "%";
+        $box2.style.left= (parseInt($box2.style.left) - 100) + "%";
+        $box3.style.left= (parseInt($box3.style.left) - 100) + "%";
+        $box4.style.left= (parseInt($box4.style.left) - 100) + "%";
+        if (parseInt($box1.style.left) == -300){
+            btn.type = "submit"
+            btn.innerHTML = "公開"
+            btn.className = "form-btn"
+            
+        }
+        else if(parseInt($box1.style.left) != -300){
+            btn.type = "button"
+            btn.innerHTML= "次"
+        }
+        /*  
+            
+        
+            box1が-300の時
+            次へボタンが送信にテキストを変更
+            タイプをsubmitに変更 */
+        /* $box1.style.left= ($left1 - 100) + "%";
+        $box2.style.left= ($left2 - 100) + "%";
+        $box3.style.left= ($left3 - 100) + "%";
+        $box4.style.left= ($left4 - 100) + "%"; */
+        console.log($box1.style.left);
+        });
+    </script>
 </body>
 </html>
