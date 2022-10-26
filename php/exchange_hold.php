614d157dcef8e89d02802ef53685d65a2308977f
takuma <kd1206659@st.kobedenshi.ac.jp>
2022-10-26 12:17:51 +0900

    終了日部分修正

--- php/exchange_hold.php
+++ php/exchange_hold.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/header.php';
 // trade.phpを読み込む
 require_once __DIR__ . '/classes/trade.php';
-$trade = New Trade();
+$trade = new Trade();
 // groupdetail.phpを読み込む
 require_once __DIR__ . '/classes/groupdetail.php';
 $group = new GroupDetail();
@@ -19,7 +19,7 @@ $msg = "";
 // #1段階では、グループIDは固定値
 $groupId = 1;
 // グループIDがあるか確認
-if(empty($groupId)){
+if (empty($groupId)) {
     $msg = "このグループでは<br>交換会を開催することは<br>出来ません。";
 }
 
@@ -27,14 +27,14 @@ if(empty($groupId)){
 $tradeInfo = $trade->gettradeInfo($groupId);
 
 // 過去の交換会情報が無い場合（交換会が開催出来る場合）
-if(empty($tradeInfo)){
+if (empty($tradeInfo)) {
     // // 現在の日付が、開催期間であるか
     // $current_date = date("Y-m-d");
     // if($tradeInfo['begin_date'] <= $current_date && $current_date <= $tradeInfo['end_date']){
     //     $msg = '現在このグループでは交換会が開催されており、<br>新たに開催することは出来ません。';
     // }
     // 開催するボタンが押されたとき
-    if(isset($_POST['hold'])){
+    if (isset($_POST['hold'])) {
         // 交換会名前と終了日を格納
         $trade_name = $_POST['trade_name'];
         $end_date = $_POST['date-max'];
@@ -42,38 +42,38 @@ if(empty($tradeInfo)){
         $trade_explain = NULL;
         $theme1 = $theme2 = $theme3 = NULL;
         // 交換会ボタンが押されているとき
-        if($_POST['explain_check'] == 'Ok') {
-            if(!empty($_POST['explain'])){
+        if ($_POST['explain_check'] == 'Ok') {
+            if (!empty($_POST['explain'])) {
                 $trade_explain = $_POST['explain'];
             }
         }
         // テーマボタンが押されているとき
-        if($_POST['theme_check'] == 'Ok'){
-            $theme_name = filter_input(INPUT_POST, 'theme', FILTER_DEFAULT,FILTER_REQUIRE_ARRAY);
-            for($i = 0; $i < 3; $i++){
-                if(!empty($theme_name[$i])){
-                    ${"theme". ($i + 1)} = $theme_name[$i];
+        if ($_POST['theme_check'] == 'Ok') {
+            $theme_name = filter_input(INPUT_POST, 'theme', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+            for ($i = 0; $i < 3; $i++) {
+                if (!empty($theme_name[$i])) {
+                    ${"theme" . ($i + 1)} = $theme_name[$i];
                 }
             }
         }
-    
+
         // 開催に必要な情報をtradeテーブルに挿入
         $trade_msg = $trade->createTrade($groupId, $trade_name, $theme1, $theme2, $theme3, $trade_explain, $end_date);
         // tradeテーブルに追加出来たか判定
-        if(empty($trade_msg)){
+        if (empty($trade_msg)) {
             // #2では交換会詳細画面に遷移する
             header("Location: exchange_hold.php");
             exit;
-        }else{
+        } else {
             $msg = $trade_msg;
         }
     }
 
-// 過去の交換会情報がある場合
-}else{
+    // 過去の交換会情報がある場合
+} else {
     $current_date = date("Y-m-d");
     // 現在の日付が、開催期間であるか
-    if($tradeInfo['begin_date'] <= $current_date && $current_date <= $tradeInfo['end_date']){
+    if ($tradeInfo['begin_date'] <= $current_date && $current_date <= $tradeInfo['end_date']) {
         $msg = '現在このグループでは<br>交換会が開催されており、<br>新たに開催することは出来ません。';
     }
 }
@@ -89,63 +89,74 @@ if(empty($tradeInfo)){
 </head>
 
 <body>
-    <?php 
-        // エラーメッセージがある場合
-        if(!empty($msg)){ ?>
-            <div class ="prompt_2">
-                <h4 class="msg-size"><?php echo $msg; ?></h4>
-            </div>
-        <?php }else{?>
-    
-    <form method="post" action="" class="exchange">
-        <!-- 交換会名入力 -->
-        <div>
-            <p class="exchange-title"><span> * </span>交換会名</p>
-            <input type="text" class=exchange-title-name name="trade_name" required>
+    <?php
+    // エラーメッセージがある場合
+    if (!empty($msg)) { ?>
+        <div class="prompt_2">
+            <h4 class="msg-size"><?php echo $msg; ?></h4>
         </div>
+    <?php } else { ?>
 
-        <!-- テーマ入力 -->
-        <div>
-            <p class="exchange-theme">テーマ</p>
-            <input type="hidden" id="theme_check" name="theme_check" value="Ok">
-            <input class="exchange-exit" type="radio" id="disp" name="theme" onclick="buttonClick_theme()" checked>あり
-            <input class="exchange-none" type="radio" id="hide" name="theme" onclick="buttonClick_theme()">なし
-            <div id="sub-form">
-                <p>交換会で交換する物のテーマを入力してください（最大3つ）</p>
-                <div id="inputArea">
-                    <input type="text" name="theme[]" class="exchange-theme-area" placeholder="3000円以下、身に着けるもの、季節もの 等">
-                    <button type="button" id="add" class="exchanege-theme-button">追加</button>
-                    <button type="button" id="del" class="exchanege-theme-button">削除</button>
+        <form method="post" action="" class="exchange">
+            <!-- 交換会名入力 -->
+            <div>
+                <p class="exchange-title"><span> * </span>交換会名</p>
+                <input type="text" class=exchange-title-name name="trade_name" required>
+            </div>
+
+            <!-- テーマ入力 -->
+            <div>
+                <p class="exchange-theme">テーマ</p>
+                <input type="hidden" id="theme_check" name="theme_check" value="Ok">
+                <input class="exchange-exit" type="radio" id="disp" name="theme" onclick="buttonClick_theme()" checked>あり
+                <input class="exchange-none" type="radio" id="hide" name="theme" onclick="buttonClick_theme()">なし
+                <div id="sub-form">
+                    <p>交換会で交換する物のテーマを入力してください（最大3つ）</p>
+                    <div id="inputArea">
+                        <input type="text" name="theme[]" class="exchange-theme-area" placeholder="3000円以下、身に着けるもの、季節もの 等">
+                        <button type="button" id="add" class="exchanege-theme-button">追加</button>
+                        <button type="button" id="del" class="exchanege-theme-button">削除</button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <!-- 説明入力 -->
-        <div>
-            <p class="exchange-explain">説明文</p>
-            <input type="hidden" id="explain_check" name="explain_check" value="Ok">
-            <input class="exchange-exit" type="radio" id="explain-disp" name="explain" onclick="buttonClick_explain()" checked>あり
-            <input class="exchange-none" type="radio" id="explain-hide" name="explain" onclick="buttonClick_explain()">なし
-            <div id="explain-form">
-                <p>説明文を入力してください</p>
-                <textarea class="exchange-explain-area" id="explain-area" wrap="hard" name="explain"></textarea><br>
+            <!-- 説明入力 -->
+            <div>
+                <p class="exchange-explain">説明文</p>
+                <input type="hidden" id="explain_check" name="explain_check" value="Ok">
+                <input class="exchange-exit" type="radio" id="explain-disp" name="explain" onclick="buttonClick_explain()" checked>あり
+                <input class="exchange-none" type="radio" id="explain-hide" name="explain" onclick="buttonClick_explain()">なし
+                <div id="explain-form">
+                    <p>説明文を入力してください</p>
+                    <textarea class="exchange-explain-area" id="explain-area" wrap="hard" name="explain"></textarea><br>
+                </div>
             </div>
-        </div>
 
-        <!-- 終了日入力 -->
-        <div>
-            <p class="exchange-finish"><span> * </span>終了日（最大4週間）</p>
-            <input class="exchange-calendar" type="date" name="date-max" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-m-d', strtotime('next month')); ?>" required></input><br>
-        </div>
+            <!-- 終了日入力 -->
+            <!-- <div>
+                <p class="exchange-finish"><span> * </span>終了日（最大4週間）</p>
+                <input class="exchange-calendar" type="date" name="date-max" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-m-d', strtotime('next month')); ?>" required></input><br>
+            </div> -->
+            <div>
+                <p class="exchange-finish"><span> * </span>終了日（最大3か月）</p>
+                <p>開催期間終了日を下記からひとつ選択してください</p>
+                <p>今日: <span id="today" class="exchange-tody"></span></p>
+                <input type="radio" name="finishday" value="1週間後" class="exchange-finishday">1週間後: <span id="after1week"></span><br>
+                <input type="radio" name="finishday" value="2週間後" class="exchange-finishday">2週間後: <span id="after2weeks"></span><br>
+                <input type="radio" name="finishday" value="3週間後" class="exchange-finishday">3週間後: <span id="after3weeks"></span><br>
+                <input type="radio" name="finishday" value="1か月後" class="exchange-finishday">1か月後: <span id="after1month"></span><br>
+                <input type="radio" name="finishday" value="2か月後" class="exchange-finishday">2か月後: <span id="after2months"></span><br>
+                <input type="radio" name="finishday" value="3か月後" class="exchange-finishday">3か月後: <span id="after3months"></span><br>
+            </div>
 
-        <div>
-            <input type="submit" class="hold-button" value="開催" name="hold">
-        </div>
+            <div>
+                <input type="submit" class="hold-button" value="開催" name="hold">
+            </div>
 
 
-        <script src="https://code.jquery.com/jquery.min.js"></script>
-        <script src="../js/exchange_hold.js"></script>
-    </form>
+            <script src="https://code.jquery.com/jquery.min.js"></script>
+            <script src="../js/exchange_hold.js"></script>
+        </form>
 
     <?php } ?>
 </body>
