a977a1c64ccb39900bdc406b01c4eb02f2064c04
Mitsui Hayato <showdown3218810@gmail.com>
2022-10-27 13:18:56 +0900

    fixed tradeinfo

--- php/tradeinfo.php
+++ php/tradeinfo.php
@@ -3,17 +3,17 @@
     require_once __DIR__ . '/header.php';
 ?>
 
-<link rel="stylesheet" href="../css/test.css">
+<link rel="stylesheet" href="../css/tradeinfo.css">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>交換会名を出力</title>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 </head>
-<body>
+<br>
     <div class="trade">
         <div class="trade-info">
             <!-- 交換会名と開催期間を表示 -->
             <div class="trade-title">
-                <h1>タイトルタイトルタイトル</h1>
+                <h1>タイトルタイトルタイトルタイトルタイトルたいとるタイトルタイ</h1>
                 <p>22/11/25 ～ 22/11/26</p>
             </div>
 
@@ -21,7 +21,7 @@
             <div class="participant">
                 <h3>交換会の参加者</h3>
                 <ul>
-                    <?php for($i = 0; $i < 10; $i++){ ?>
+                    <?php for($i = 0; $i < 2; $i++){ ?>
                     <li>
                         <img class="img-icon" src="../static/user_icon.png" alt="">
                     </li>
@@ -35,9 +35,9 @@
                 <p>テーマに適する物を交換会に出しましょう！</p>
                 <div class="readmore">
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>テーマテーマテーマテーマテーマテーマテーマテーマテーマテーマ</li>
+                        <li>テーマテーマテーマテーマテーマテーマテーマテーマテーマテーマ</li>
+                        <li>テーマテーマテーマテーマテーマテーマテーマテーマテーマテーマ</li>
                     </ul>
                 </div>
             </div>
@@ -61,6 +61,7 @@
         <form method="POST" action="" class="trade-form" enctype="multipart/form-data">
             <h2>交換会に参加してみましょう！</h2>
             <div class="form-image">
+                <h3><span> * </span>交換物の画像（1枚まで）</h3>
                 <div id="sample-img" class="sample-img"></div>
                 <label class="upload-label">
                     画像を選択
@@ -69,12 +70,15 @@
             </div>
 
             <div class="form-name">
+                <h3><span> * </span>交換物の名前（30文字まで）</h3>
                 <input type="text" class="form-box" name="goods_name" maxlength="30" value="" required>
                 <input type="text" style="display: none;"/>
             </div>
 
             <div class="form-hint">
-                <input type="text" class="form-box" name="goods_hint" maxlength="30" value="">
+                <h3>ヒント（30文字まで）</h3>
+                <p>交換されるまでメンバーに表示される交換物のヒントを書こう！</p>
+                <input type="text" class="form-box" name="goods_hint" maxlength="30" value="" placeholder="（例）形・色の特徴など">
             </div>
 
             <input type="submit" name="post_btn" class="tradeinfo-button" value="参加する"></input>
