230d3c0cda84f2bfc7f0302d6bcc5ea1d17f207e
YokotaAtsuyakd <kd1248793@st.kobedenshi.ac.jp>
2022-10-31 13:56:42 +0900

    文章修正

--- php/notification.php
+++ php/notification.php
@@ -37,7 +37,7 @@
                     echo '<div class="title-top"><h1 class="title-content">１か月以内</h1></div>';
                 } else if($date < date("Y-m-d",strtotime("-1 month")) && $dc < 5){
                     $dc = 5;
-                    echo '<div class="title-top"><h1 class="title-content">以前</h1></div>';
+                    echo '<div class="title-top"><h1 class="title-content">１か月以上前</h1></div>';
                 }
 
                 //　既読か判定
@@ -55,43 +55,43 @@
                         echo "href='gift_detail.php?id={$view['gift_id']}'>";
                         echo "<div class='notifi-img-back'><img class='notifi-img' src='data:;base64,{$img}'></div>";
                         echo "<div class='notifi-vertical'><p class='notif-time'>{$view['time']}</p><p class='notif-comment'>";
-                        echo "{$view['name']}から{$view['gift_name']}に{$view['pattern_name']}が来ています。";
+                        echo "{$view['name']}から{$view['gift_name']}に申請が来ています。";
                         break;
                     case 2:
                         echo "href='#'>";
                         echo "<div class='notifi-img-back'><img class='notifi-img' src='data:;base64,{$img}'></div>";
                         echo "<div class='notifi-vertical'><p class='notif-time'>{$view['time']}</p><p class='notif-comment'>";
-                        echo "{$view['groupname']}の交換会が{$view['pattern_name']}されました。";
+                        echo "{$view['groupname']}の交換会が開催されました。";
                         break;
                     case 3:
                         echo "href='#'>";
                         echo "<div class='notifi-img-back'><img class='notifi-img' src='data:;base64,{$img}'></div>";
                         echo "<div class='notifi-vertical'><p class='notif-time'>{$view['time']}</p><p class='notif-comment'>";
-                        echo "{$view['groupname']}の交換会が{$view['pattern_name']}しました。";
+                        echo "{$view['groupname']}の交換会が終了しました。";
                         break;
                     case 4:
                         echo "href='gift_detail.php?id={$view['gift_id']}'>";
                         echo "<div class='notifi-img-back'><img class='notifi-img' src='data:;base64,{$img}'></div>";
                         echo "<div class='notifi-vertical'><p class='notif-time'>{$view['time']}</p><p class='notif-comment'>";
-                        echo "{$view['name']}から{$view['gift_name']}に{$view['pattern_name']}されました。";
+                        echo "{$view['name']}が{$view['gift_name']}をお気に入りにしました。";
                         break;
                     case 5:
                         echo "href='gift_detail.php?id={$view['gift_id']}'>";
                         echo "<div class='notifi-img-back'><img class='notifi-img' src='data:;base64,{$img}'></div>";
                         echo "<div class='notifi-vertical'><p class='notif-time'>{$view['time']}</p><p class='notif-comment'>";
-                        echo "{$view['name']}から{$view['gift_name']}に{$view['count']}件の{$view['pattern_name']}が届いています。";
+                        echo "{$view['name']}から{$view['gift_name']}に{$view['count']}件のコメントが届いています。";
                         break;
                     case 6:
                         echo "href='#'>";
                         echo "<div class='notifi-img-back'><img class='notifi-img' src='data:;base64,{$img}'></div>";
                         echo "<div class='notifi-vertical'><p class='notif-time'>{$view['time']}</p><p class='notif-comment'>";
-                        echo "{$view['gift_name']}の{$view['pattern_name']}が切れました。";
+                        echo "{$view['gift_name']}の期限が切れました。";
                         break;
                     case 7:
                         echo "href='group.php?groupid={$view['group_send']}'>";
                         echo "<div class='notifi-img-back'><img class='notifi-img' src='data:;base64,{$img}'></div>";
                         echo "<div class='notifi-vertical'><p class='notif-time'>{$view['time']}</p><p class='notif-comment'>";
-                        echo "{$view['name']}が{$view['groupname']}に{$view['pattern_name']}しました。";
+                        echo "{$view['name']}が{$view['groupname']}に参加しました。";
                         break;
                 }
                 echo "</p></div></a><hr class='notifi-border'>";
