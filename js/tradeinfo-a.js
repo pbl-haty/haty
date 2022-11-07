d9b28f649ab41e1fee5ba986fa9da840c7a26c0a
Mitsui Hayato <showdown3218810@gmail.com>
2022-11-07 23:12:03 +0900

    completed most function after_post_trade

--- js/tradeinfo-a.js
+++ js/tradeinfo-a.js
@@ -10,15 +10,15 @@ b1.style.border ="2px gray solid";
 //                 btn.textContent = "完了しました";
 //             })
 // }
-function click_list_event(i) {
-    if(i == 0) {
+function event(i) {
+    if(i == 1) {
         p0.style.display ="block";
         p1.style.display ="none";
         b0.style.color ="black";
         b1.style.color ="gray";
         b0.style.border ="2px black solid";
         b1.style.border ="2px gray solid";
-    } else if(i == 1) {
+    } else if(i == 2) {
         p0.style.display ="none";
         p1.style.display ="block";
         b0.style.color ="gray";
