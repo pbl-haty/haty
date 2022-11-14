2a2cc8291a05f4ee888bf28c8192076856ac20e0
Mitsui Hayato <showdown3218810@gmail.com>
2022-11-14 21:33:00 +0900

    finish main function

--- php/trade_list.php
+++ php/trade_list.php
@@ -5,11 +5,13 @@
     $user = new User();
     require_once __DIR__ . '/classes/trade.php';
     $trade = new Trade();
+    require_once __DIR__ . '/classes/groupmember.php';
+    $groupmember = new GroupMember();
     // セッションからログイン中のユーザーIDを取得する
     $userid = $_SESSION['uid'];
 
     $current_trade_array = [];
-    $finish_trade_array = [];
+    $past_trade_array = [];
     $current_date = date("Y-m-d");
 
     // ユーザーIDから所属しているグループIDを取得する
@@ -20,7 +22,7 @@
             if($trade_info['begin_date'] <= $current_date && $current_date <= $trade_info['end_date']){
                 array_push($current_trade_array, $trade_info); 
             }else{
-                array_push($finish_trade_array, $trade_info);
+                array_push($past_trade_array, $trade_info);
             }
         }
     }
@@ -31,36 +33,52 @@
 
 <body>
     <br>
-    <div class="trade-list"></div>
+    <div class="trade-list">
         <div class="tab-panel">
             <!--タブ-->
-                <div class="tab-group">
-                    <div class="tab tab-A is-active">開催中の交換会</div>
-                    <div class="tab tab-B">過去の交換会</div>
-                </div>
+            <div class="tab-group">
+                <div class="tab tab-A is-active">開催中の交換会</div>
+                <div class="tab tab-B">過去の交換会</div>
+            </div>
 
-                <p><?php $current_trade_array ?></p>
         
             <!--タブを切り替えて表示するコンテンツ-->
-            <div class="panel-group">
-                <div class="panel tab-A is-show">
-                    <div class="group-icon-space">
-                        <img src="" alt="">
+            <div class="panel tab-A is-show">
+                <?php foreach ($current_trade_array as $current_trade){ ?>
+                <a href="tradeinfo.php?trade_id=<?php echo $current_trade['trade_id']; ?>" class="panel-group">
+                    <div class="group-icon-space"> 
+                        <?php
+                        $group_info = $groupmember->groupconf($current_trade['group_id']);
+                        $group_icon_image = base64_encode($group_info['icon']);
+                        ?>
+                        <img src="data:;base64,<?php echo $group_icon_image; ?>" class="group-icon">
                     </div>
                     <div class="trade-info-space">
-
+                        <p class="trade-name"><?php echo $current_trade['trade_name'] ?></p>
+                        <p class="trade-date"><?php echo $current_trade['begin_date'];?> ～ <?php echo $current_trade['end_date'];?></p>
                     </div>
-                </div>
-                <div class="panel tab-B">
-                    <div class="panel tab-A is-show">
-                        <div class="group-icon-space">
-                            <img src="" alt="">
-                        </div>
-                        <div class="trade-info-space">
-
-                        </div>
+                </a>
+                <hr class="border">
+                <?php } ?>
+            </div>
+            
+            <div class="panel tab-B">
+                <?php foreach ($past_trade_array as $past_trade){ ?>
+                <a href="tradeinfo.php?trade_id=<?php echo $past_trade['trade_id']; ?>" class="panel-group">
+                    <div class="group-icon-space">
+                        <?php
+                        $group_info = $groupmember->groupconf($past_trade['group_id']);
+                        $group_icon_image = base64_encode($group_info['icon']);
+                        ?>
+                        <img src="data:;base64,<?php echo $group_icon_image; ?>" class="group-icon">
                     </div>
-                </div>
+                    <div class="trade-info-space">
+                        <p class="trade-name"><?php echo $past_trade['trade_name']; ?></p>
+                        <p class="trade-date"><?php echo $past_trade['begin_date'];?> ～ <?php echo $past_trade['end_date'];?></p>
+                    </div>
+                </a>
+                <hr class="border">
+                <?php } ?>
             </div>
         </div>
     </div>
