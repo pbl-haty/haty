f2c2355b92d2e2d66febafcc5318a724deb6ee6b
YAMATO Matsuoka <kd1283950@st.kobedenshi.ac.jp>
2022-11-21 14:23:00 +0900

    途中経過2

--- php/tradeinfo_c.php
+++ php/tradeinfo_c.php
@@ -8,7 +8,10 @@
     <div class="tradeinfo-edit">
         <h2>投稿商品修正</h2>
         <div class="form-image">
-            <h3><span> * </span>交換物の画像（1枚まで）</h3>
+            <div class="trade-box">
+                <h3 class="trade-left"><span> * </span>交換画像</h3>
+                <p class="trade-right">最大1枚</p>
+            </div>
             <div id="sample-img" class="sample-img">
                 <img class="sample-img-size" src="../static\user_icon.png">
             </div>
@@ -19,13 +22,19 @@
         </div>
 
         <div class="form-name">
-            <h3><span> * </span>交換物の名前（30文字まで）</h3>
+            <div class="trade-box">
+                <h3 class="trade-left"><span> * </span>交換名</h3>
+                <p class="trade-right">最大30文字</p>
+            </div>
             <input type="text" class="form-box" name="goods_name" maxlength="30" value="まんとる" required>
             <input type="text" style="display: none;"/>
         </div>
 
         <div class="form-hint">
-            <h3>ヒント（30文字まで）</h3>
+            <div class="trade-box">
+                <h3 class="trade-left">ヒント</h3>
+                <p class="trade-right">最大30文字</p>
+            </div>
             <p>交換されるまでメンバーに表示される交換物のヒントを書こう！</p>
             <input type="text" class="form-box" name="goods_hint" maxlength="30" value="でかい" placeholder="（例）形・色の特徴など">
         </div>
