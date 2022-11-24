11dd60162867cc932de85cba80473e9301b278ef
YAMATO Matsuoka <kd1283950@st.kobedenshi.ac.jp>
2022-11-24 11:35:50 +0900

    修正

--- php/header.php
+++ php/header.php
@@ -23,7 +23,7 @@ $count = $notifi->notifi_count($userId);
         <!-- <h1 class="title">HATY</h1> -->
     </a>
 
-    <a class="form-btn" href="https://docs.google.com/forms/d/e/1FAIpQLScE6T7LzlXGWWxMa6WTjlpiVezTVhg2LNobXS3eFHRUi0cD1A/viewform?usp=sharing">
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLScE6T7LzlXGWWxMa6WTjlpiVezTVhg2LNobXS3eFHRUi0cD1A/viewform?usp=sharing">
         <img class="notification-a" style="margin-left: 200px;" src="../static/form-btn.png">
     </a>
 
