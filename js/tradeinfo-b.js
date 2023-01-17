42065eee0cff9fd31f41e48b5fa143aff40b5aba
takuma <kd1206659@st.kobedenshi.ac.jp>
2023-01-17 12:30:24 +0900

    インフォメーションボタン

--- js/tradeinfo-b.js
+++ js/tradeinfo-b.js
@@ -16,7 +16,7 @@ function click_icon_event(i) {
 $(document).on('click', function(e) {
 	// ２．クリックされた場所の判定
 	if(!$(e.target).closest('#pop_up').length && !$(e.target).closest('#button').length){
-		$('#pop_up').fadeOut();
+		$('#pop_up').fadeOut(0);
 	}else if($(e.target).closest('#button').length){
 		// ３．ポップアップの表示状態の判定
 		if($('#pop_up').is(':hidden')){
